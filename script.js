4f9649528b0b478996aa296a3d9e5d69f5401e96
TimeApollo <arnjcksn0845@yahoo.com>
2018-05-01 15:13:28 -0400

    Finished baseline of project

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("findButton").onclick = function() {
+    let typedText = document.getElementById("input").value;
+    let organizedTypedText = alphabetize(typedText);
+    var anaString = []
+    for (let i = 0; i<words.length;i++){
+        let organizedWords = alphabetize(words[i]);
+        if(organizedTypedText === organizedWords){
+            anaString.push(words[i]);
+        }
+    }
+    //console.log(anaString);
+    let printAnaString = anaString.join(" ");
+    //console.log(printAnaString);
+    let anagramResult = document.createElement("p");
+    anagramResult.textContent = printAnaString;
+    document.body.appendChild(anagramResult);
+}
+
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
